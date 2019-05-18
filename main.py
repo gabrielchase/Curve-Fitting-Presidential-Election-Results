162a80646d9577e97fb435dead2bf831716083b1
gabrielchase <gchase.patron@gmail.com>
2019-05-18 18:55:42 +0800

    Compute correlation coefficient

--- main.py
+++ main.py
@@ -10,9 +10,55 @@ import statistics
 import matplotlib.pyplot as plt
 import numpy as np
 
-REGRESSION_TYPE = "QUADRATIC"
+REGRESSION_TYPE = "LINEAR"
 
+def get_correlation_coefficient(_type, list_1, list_2):
+    assert len(list_1) == len(list_2)
+    n = len(list_1)
+    correlation_coefficient = 0
 
+    if _type == 'LINEAR':
+        x_list = list_1
+        y_list = list_2 
+
+        summation_x_y = 0
+        for x, y in zip(x_list, y_list):
+            summation_x_y += x * y 
+
+        summation_x = sum(x_list)
+        summation_y = sum(y_list)
+
+        summation_x_squared = 0
+        for x in x_list:
+            summation_x_squared += x**2
+
+        summation_x_squared = 0
+        for x in x_list:
+            summation_x_squared += x**2
+
+        summation_y_squared = 0
+        for y in y_list:
+            summation_y_squared += y**2
+
+        numerator = (n * summation_x_y) - (summation_x * summation_y)
+        denominator = math.sqrt(((n * summation_x_squared) - (summation_x**2)) * ((n * summation_y_squared) - (summation_y**2)))
+
+        correlation_coefficient = numerator / denominator
+    elif _type in ['EXPONENTIAL', 'QUADRATIC']:
+        y_list = list_1 
+        y_predicted = list_2
+        y_bar = np.mean(y_list)
+
+        summation_y_minus_y_predicted = 0
+        summation_y_minus_y_bar = 0
+        for y1, y2 in zip(y_list, y_predicted):
+            summation_y_minus_y_predicted += (y1 - y2)**2
+            summation_y_minus_y_bar += (y1 - y_bar)**2
+
+        correlation_coefficient = math.sqrt((1 - (summation_y_minus_y_predicted/summation_y_minus_y_bar)))
+
+    return correlation_coefficient
+        
 def least_squares_quadratic(x_list, y_list):
     assert len(x_list) == len(y_list)
     n = len(x_list)
@@ -174,8 +220,6 @@ def create_plot(plot_data, title, categories, _type):
     regression_line_equation = ""
     
     for i, (ipd, color, group) in enumerate(zip(plot_data, colors, groups)):
-        print(i)
-        print("ipd: ", ipd)
         # Plots X and Y points in the scatter plot
         if i != 2:
             x, y = ipd
@@ -183,31 +227,47 @@ def create_plot(plot_data, title, categories, _type):
 
         # Plots the linear regression line
         if i == 2:
-            print("in 2")
             # Get X values for linear regression and save into x_plot
             all_x = plot_data[0][0] + plot_data[1][0]
+            # print('X: ', all_x)
             x_range = max(all_x) - min(all_x)
             x_increments = x_range / len(all_x)
             x_plot = list(range(0, int(max(all_x)), int(x_increments)))
             
             # Get Y values for linear regression
+            all_y = plot_data[0][1] + plot_data[1][1]
+            # print('Y: ', all_y)
             y_plot = []
             
             if _type == "LINEAR":
                 a, b = ipd
                 y_plot = [a + (b * _x) for _x in x_plot]
                 regression_line_equation = "y = {}x + {}".format(b, a)
+                correlation_coefficient = get_correlation_coefficient(REGRESSION_TYPE, all_x, all_y)
+                
+                print('Regression Equation: ', regression_line_equation)
+                print('R: ', correlation_coefficient)
             elif _type == "EXPONENTIAL":
                 a, b = ipd
                 y_plot = [math.e**(a + b * _x) for _x in x_plot]
+                y_predicted = [math.e**(a + b * _x) for _x in all_x]
                 regression_line_equation = "y = e^({} + {}x)".format(a, b)
+                correlation_coefficient = get_correlation_coefficient(REGRESSION_TYPE, all_y, y_predicted)
+                
+                print('Regression Equation: ', regression_line_equation)
+                print('R: ', correlation_coefficient)
             elif _type == "QUADRATIC":
                 a, b, c = ipd 
                 y_plot = [a*(_x**2) +  b*_x + c for _x in x_plot]
+                y_predicted = [a*(_x**2) +  b*_x + c for _x in all_x]
                 regression_line_equation = "y = {}x^2 + {}x + {} ".format(a, b, c)
+                correlation_coefficient = get_correlation_coefficient(REGRESSION_TYPE, all_y, y_predicted)
+                
+                print('Regression Equation: ', regression_line_equation)
+                print('R: ', correlation_coefficient)
 
             plt.plot(x_plot, y_plot, "m-")
-
+    
     plt.ylabel(categories["y"])
     plt.xlabel("{}\n{}".format(categories["x"], regression_line_equation))
     plt.title(title)
@@ -229,10 +289,10 @@ if __name__ == "__main__":
 
     plot_data = get_data(json_data, REGRESSION_TYPE, **categories)
 
-    print("Republican {}: ".format(categories["x"]), plot_data[0][0])
-    print("Republican {}: ".format(categories["y"]), plot_data[0][1])
+    # print("Republican {}: ".format(categories["x"]), plot_data[0][0])
+    # print("Republican {}: ".format(categories["y"]), plot_data[0][1])
 
-    print("Democrat {}: ".format(categories["x"]), plot_data[1][0])
-    print("Democrat {}: ".format(categories["y"]), plot_data[1][1])
+    # print("Democrat {}: ".format(categories["x"]), plot_data[1][0])
+    # print("Democrat {}: ".format(categories["y"]), plot_data[1][1])
 
     create_plot(plot_data, "Candidate {} vs. {}".format(categories["x"], categories["y"]), categories, REGRESSION_TYPE)
